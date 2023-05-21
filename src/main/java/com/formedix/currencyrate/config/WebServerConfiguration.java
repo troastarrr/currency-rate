a032c33669055b857b40c602a069ff1d0f497b34
Clarc Infante <clarence.infante@quincus.com>
2023-05-21 19:49:55 +0800

    Externalize uri path

--- src/main/java/com/formedix/currencyrate/config/WebServerConfiguration.java
+++ src/main/java/com/formedix/currencyrate/config/WebServerConfiguration.java
@@ -1,5 +1,6 @@
 package com.formedix.currencyrate.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +8,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebServerConfiguration {
+    @Value("${root-uri}")
+    private String rootUri;
 
     @Bean
     public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
-        return factory -> factory.setContextPath("/formedix");
+        return factory -> factory.setContextPath(rootUri);
     }
 }
